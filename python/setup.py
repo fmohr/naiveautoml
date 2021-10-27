7d4c2ed3dd658ddc66ce89df942e7dc2842df519
Felix <fmohr@mail.upb.de>
2021-10-27 17:40:06 +0200

    added searchspace.json

--- python/setup.py
+++ python/setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'naiveautoml',         # How you named your package folder (MyLib)
   packages = ['naiveautoml'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'The official package for the Naive AutoML paper',   # Give a short description about your library
   author = 'Felix Mohr',                   # Type in your name
   author_email = 'mail@felixmohr.de',      # Type in your E-Mail
   url = 'https://github.com/fmohr/naiveautoml',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/fmohr/naiveautoml/archive/refs/tags/v0.1.tar.gz',
+  download_url = 'https://github.com/fmohr/naiveautoml/archive/refs/tags/v0.0.1.tar.gz',
   keywords = ['AutoML', 'sklearn', 'naive', 'simple'],
   install_requires=[
           'numpy',
@@ -29,5 +29,6 @@ setup(
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9'
   ],
-  package_data={'': ['searchspace.json']}
+  package_data={'': ['searchspace.json']},
+  include_package_data=True
 )
