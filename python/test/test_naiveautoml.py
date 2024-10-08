4f1f1d5ae00ce6695c5ca12244cc080d2e9cc5d4
felix <felix@frank>
2024-10-08 04:27:05 +0200

    fixed issue in test_searchspace unit test

--- python/test/test_naiveautoml.py
+++ python/test/test_naiveautoml.py
@@ -808,7 +808,8 @@ class TestNaiveAutoML(unittest.TestCase):
                                 "ValueError: array must not contain infs or NaNs",
                                 "ValueError: Input X contains infinity or a value too large for",
                                 "ValueError: illegal value in 4th argument of internal gesdd",
-                                "ValueError: Found array with 0 feature(s)"
+                                "ValueError: Found array with 0 feature(s)",
+                                "Expected n_neighbors <= n_samples_fit, but n_neighbors"
                             ]
                             if not any([t in exception for t in allowed_exception_texts]):
                                 self.logger.exception(exception)
